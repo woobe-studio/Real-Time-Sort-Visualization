89ad102cedd14b3c730a2cabdfff934497d8f8ca
Michael Wozniak <woobe-studio@proton.me>
2024-07-26 09:59:57 +0200

    added bubble sort visualization

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,214 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
+import 'dart:math';
 
 void main() {
-  runApp(const MyApp());
+  runApp(SortComparisonApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
+class SortComparisonApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Sort Comparison',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: SortComparisonPage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class SortComparisonPage extends StatefulWidget {
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _SortComparisonPageState createState() => _SortComparisonPageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _SortComparisonPageState extends State<SortComparisonPage> {
+  List<int> _bubbleSortNumbers = [];
+  List<int> _quickSortNumbers = [];
+  bool _isSorting = false;
+  bool _cancelSorting = false;
+  int _duration = 1;
 
-  void _incrementCounter() {
+  @override
+  void initState() {
+    super.initState();
+    _generateRandomNumbers();
+  }
+
+  void _generateRandomNumbers() {
+    _bubbleSortNumbers = List.generate(100, (index) => index + 1)..shuffle();
+    _quickSortNumbers = List.from(_bubbleSortNumbers);
+    setState(() {});
+  }
+
+  Future<void> _bubbleSort() async {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _isSorting = true;
+      _cancelSorting = false;
     });
+
+    for (int i = 0; i < _bubbleSortNumbers.length - 1; i++) {
+      if (_cancelSorting) {
+        setState(() {
+          _isSorting = false;
+          _cancelSorting = false;
+        });
+        return;
+      }
+      for (int j = 0; j < _bubbleSortNumbers.length - i - 1; j++) {
+        if (_cancelSorting) {
+          setState(() {
+            _isSorting = false;
+            _cancelSorting = false;
+          });
+          return;
+        }
+        if (_bubbleSortNumbers[j] > _bubbleSortNumbers[j + 1]) {
+          int temp = _bubbleSortNumbers[j];
+          _bubbleSortNumbers[j] = _bubbleSortNumbers[j + 1];
+          _bubbleSortNumbers[j + 1] = temp;
+          setState(() {});
+          await Future.delayed(Duration(milliseconds: _duration));
+        }
+      }
+    }
+
+    setState(() {
+      _isSorting = false;
+    });
+  }
+
+  Future<void> _quickSort(List<int> numbers, int low, int high) async {
+    if (low < high) {
+      int pi = await _partition(numbers, low, high);
+      await _quickSort(numbers, low, pi - 1);
+      await _quickSort(numbers, pi + 1, high);
+      setState(() {});
+    }
+  }
+
+  Future<int> _partition(List<int> numbers, int low, int high) async {
+    int pivot = numbers[high];
+    int i = low - 1;
+
+    for (int j = low; j < high; j++) {
+      if (numbers[j] < pivot) {
+        i++;
+        int temp = numbers[i];
+        numbers[i] = numbers[j];
+        numbers[j] = temp;
+        setState(() {});
+        await Future.delayed(Duration(milliseconds: _duration));
+      }
+    }
+
+    int temp = numbers[i + 1];
+    numbers[i + 1] = numbers[high];
+    numbers[high] = temp;
+    setState(() {});
+    await Future.delayed(Duration(milliseconds: _duration));
+
+    return i + 1;
+  }
+
+  void _handlePlayButtonPress() {
+    if (!_isSorting) {
+      _cancelSorting = false;
+      _bubbleSort();
+      _quickSort(_quickSortNumbers, 0, _quickSortNumbers.length - 1);
+    }
+  }
+
+  void _handleRestartOrShuffleButtonPress() {
+    if (_isSorting) {
+      setState(() {
+        _cancelSorting = true;
+      });
+    } else {
+      _generateRandomNumbers();
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text('Sort Comparison'),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.shuffle),
+            onPressed: _isSorting ? null : _handleRestartOrShuffleButtonPress,
+          ),
+          IconButton(
+            icon: Icon(Icons.speed),
+            onPressed: _isSorting ? null : () {
+              setState(() {
+                _duration = _duration == 1 ? 100 : 1;
+              });
+            },
+          ),
+        ],
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      body: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Expanded(
+            child: CustomPaint(
+              painter: BarPainter(_bubbleSortNumbers),
+              child: Container(),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+          ),
+          Expanded(
+            child: CustomPaint(
+              painter: BarPainter(_quickSortNumbers),
+              child: Container(),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        onPressed: _handlePlayButtonPress,
+        child: Icon(_isSorting ? Icons.restart_alt : Icons.play_arrow),
+        tooltip: _isSorting ? 'Restart Sorting' : 'Start Sorting',
+      ),
+      persistentFooterButtons: [
+        ElevatedButton.icon(
+          onPressed: _handleRestartOrShuffleButtonPress,
+          icon: Icon(_isSorting ? Icons.stop : Icons.shuffle),
+          label: Text(_isSorting ? 'Stop Sorting' : 'Shuffle Numbers'),
+        ),
+      ],
     );
+  }
+}
+
+class BarPainter extends CustomPainter {
+  final List<int> numbers;
+
+  BarPainter(this.numbers);
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    final paint = Paint()
+      ..strokeWidth = size.width / numbers.length;
+
+    final barWidth = size.width / numbers.length;
+    for (int i = 0; i < numbers.length; i++) {
+      final barHeight = (numbers[i] / numbers.length) * size.height;
+      final color = Color.lerp(Colors.red, Colors.green, numbers[i] / numbers.length);
+      paint.color = color ?? Colors.blue;
+      canvas.drawLine(
+        Offset(i * barWidth, size.height),
+        Offset(i * barWidth, size.height - barHeight),
+        paint,
+      );
+    }
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return true;
   }
 }
